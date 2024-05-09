ac917f7ef612f8c5091f522a0fce0f3e1d70fb32
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-09 14:26:12 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment1._3._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myarray = {1,2,3,4,5,6,7,8,9};
+            int L = 0;
+            int R = myarray.Length - 1;
+            int temp = L;
+
+            while (L < R) 
+            {
+                temp = myarray[L];
+                myarray[L] = myarray[R];
+                myarray[R] = temp;
+                L++;
+                R--;
+            }
+            foreach (int i in myarray) 
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
